39733197c359e1f3c4b8aa112668193972c623a9
Apace <49096778+apace100@users.noreply.github.com>
2023-07-02 23:21:18 +0200

    Changed FabricShieldLib compat to use reflection

--- src/main/java/io/github/apace100/autotag/common/AutoTagConvention.java
+++ src/main/java/io/github/apace100/autotag/common/AutoTagConvention.java
@@ -6,8 +6,12 @@ import io.github.apace100.autotag.common.tags.AutoBlockTags;
 import io.github.apace100.autotag.common.tags.AutoItemTags;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class AutoTagConvention implements ModInitializer {
+
+	public static Logger LOGGER = LogManager.getLogger(AutoTagConvention.class);
 
 	@Override
 	public void onInitialize() {
--- src/main/java/io/github/apace100/autotag/common/compat/FabricShieldLibCompat.java
+++ src/main/java/io/github/apace100/autotag/common/compat/FabricShieldLibCompat.java
@@ -1,7 +1,7 @@
 package io.github.apace100.autotag.common.compat;
 
-import com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShield;
 import io.github.apace100.autotag.api.AutoTagRegistry;
+import io.github.apace100.autotag.common.AutoTagConvention;
 import io.github.apace100.autotag.common.TagIdentifiers;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKeys;
@@ -10,9 +10,14 @@ import net.minecraft.registry.tag.TagKey;
 public final class FabricShieldLibCompat {
 
     public static void register() {
-        AutoTagRegistry.register(
-            Registries.ITEM,
-            TagKey.of(RegistryKeys.ITEM, TagIdentifiers.Items.SHIELDS),
-            item -> item instanceof FabricShield);
+        try {
+            Class<?> fabricShieldInterface = Class.forName("com.github.crimsondawn45.fabricshieldlib.lib.object.FabricShield");
+            AutoTagRegistry.register(
+                    Registries.ITEM,
+                    TagKey.of(RegistryKeys.ITEM, TagIdentifiers.Items.SHIELDS),
+                    fabricShieldInterface::isInstance);
+        } catch (ClassNotFoundException e) {
+            AutoTagConvention.LOGGER.error("FabricShieldLib was loaded, but the interface could not be found. AutoTag might be outdated. FabricShieldLib compatibility will be disabled.");
+        }
     }
 }
