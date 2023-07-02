dc7d48c4bb282c3568ac13860d6a4690fbccbc26
Apace <49096778+apace100@users.noreply.github.com>
2023-07-02 17:34:40 +0200

    Updated to 1.19.4

--- src/main/java/io/github/apace100/autotag/common/tags/AutoBiomeTags.java
+++ src/main/java/io/github/apace100/autotag/common/tags/AutoBiomeTags.java
@@ -4,6 +4,7 @@ import io.github.apace100.autotag.api.AutoTagRegistry;
 import io.github.apace100.autotag.common.TagIdentifiers;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 
 public final class AutoBiomeTags {
@@ -11,12 +12,12 @@ public final class AutoBiomeTags {
     public static void register() {
         AutoTagRegistry.register(RegistryKeys.BIOME,
             TagKey.of(RegistryKeys.BIOME, TagIdentifiers.Biomes.PRECIPITATION_NONE),
-            biome -> biome.getPrecipitation() == Biome.Precipitation.NONE);
+            biome -> biome.getPrecipitation(BlockPos.ORIGIN) == Biome.Precipitation.NONE);
         AutoTagRegistry.register(RegistryKeys.BIOME,
             TagKey.of(RegistryKeys.BIOME, TagIdentifiers.Biomes.PRECIPITATION_RAIN),
-            biome -> biome.getPrecipitation() == Biome.Precipitation.RAIN);
+            biome -> biome.getPrecipitation(BlockPos.ORIGIN) == Biome.Precipitation.RAIN);
         AutoTagRegistry.register(RegistryKeys.BIOME,
             TagKey.of(RegistryKeys.BIOME, TagIdentifiers.Biomes.PRECIPITATION_SNOW),
-            biome -> biome.getPrecipitation() == Biome.Precipitation.SNOW);
+            biome -> biome.getPrecipitation(BlockPos.ORIGIN) == Biome.Precipitation.SNOW);
     }
 }
